cd186a08f799d47c0ed69227d2ed623c4c54af82
Corrigan <corrigangaertner@gmail.com>
2023-04-20 23:59:22 -0400

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Corrigan Gaertner on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
